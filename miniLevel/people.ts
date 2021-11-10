65f4adecebe297623f10f8932e802a0a7f458fce
Gracias claude <31932887+graciasc@users.noreply.github.com>
2021-11-10 07:36:27 -0800

    Update people.ts
    add functions

--- miniLevel/people.ts
+++ miniLevel/people.ts
@@ -6,9 +6,27 @@ type People = Array <{ [key:string]: string | number }>
 const people: People = [ { name: "Gracias", age: 40}, { name: 'Nav', age: 70}, { name: 'Billy', age: 10}, { name: 'Carrie', age: 45 } ]
 
 
-// Loop through people and return all the name of the people.
+ function allNames(people: People): string {
+    // Loop through people and return all the name of the people.
+   
+   
+ }
+
+  function allAges(people: People): number {
+    // Loop through people and return all ages.
+    
+  }
+
+  function inTwentyYears(people: People) : string {
+    // Loop through people and return what age they will be in 20 years. 
+
+  }
+
+  function getSpecificNames(people: People) :  { [key: string]: string | number } {
+   // filter out a person. js method .filter 
+    
+  }
   
-// Loop through people and return all ages.
-  
-// Loop through people and return what age they will be in 20 years. 
-  
+
+// call functions below
+
