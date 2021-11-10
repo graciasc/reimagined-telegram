6db19aa051958e5b7d12f750c4475d518b67816f
Gracias claude <31932887+graciasc@users.noreply.github.com>
2021-11-10 07:44:44 -0800

    Create palindrome.ts

---
+++ miniLevel/palindrome.ts
@@ -0,0 +1,15 @@
+// A palindrome is a word that can be flipped and represent the same word
+// ex: level, radar, wow, noon
+
+function palindromeTest( word: string) : boolean {
+
+// check if word is a palidrone and return true or false
+
+}
+
+
+// call function below
+const firstTest = palindromeTest('level')
+const secondTest = palindromeTest('Malayalam')
+
+// log results below
