7f0f6a04d6d449bd3772c47083ff9dbd760eb413
Gracias claude <31932887+graciasc@users.noreply.github.com>
2021-11-10 07:38:36 -0800

    Update people.ts

--- miniLevel/people.ts
+++ miniLevel/people.ts
@@ -5,7 +5,7 @@ type People = Array <{ [key:string]: string | number }>
 
 const people: People = [ { name: "Gracias", age: 40}, { name: 'Nav', age: 70}, { name: 'Billy', age: 10}, { name: 'Carrie', age: 45 } ]
 
-
+ // Way to read this is expects a parameter that has the shape of People and returns a string
  function allNames(people: People): string {
     // Loop through people and return all the name of the people.
    
