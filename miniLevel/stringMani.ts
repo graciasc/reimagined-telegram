a7372ee588ac20b8f08648dda7bf61e15723d2d1
Gracias claude <31932887+graciasc@users.noreply.github.com>
2021-11-10 07:58:33 -0800

    Create stringMani.ts

---
+++ miniLevel/stringMani.ts
@@ -0,0 +1,23 @@
+// Js methods that make this easier .replace .concat
+
+//remove __ and put your name
+const profession: string = 'My Name is __ and I am a developer for a living'
+
+// add this welcome to the beginning of the welcome string
+const welcome: string = 'To Whom this may concern'
+
+// remove World from string below
+const HelloWorld: string = 'Hello World'
+
+
+//intermediate
+
+// add Sir to the beginning of each string in the array
+const names: string[] = ['Billy', 'John', 'James', 'Peter']
+
+
+// with the new array return all strings in the array. Ex: Sir Billy Sir John
+
+
+
+
