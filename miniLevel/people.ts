18c66cc83df9fcc9765815e70466be3b481da493
Gracias claude <31932887+graciasc@users.noreply.github.com>
2021-11-09 23:15:47 -0700

    Create people.ts

---
+++ miniLevel/people.ts
@@ -0,0 +1,14 @@
+// Mini Level Up
+
+// Giving shape to people
+type People = Array <{ [key:string]: string | number }>
+
+const people: People = [ { name: "Gracias", age: 40}, { name: 'Nav', age: 70}, { name: 'Billy', age: 10}, { name: 'Carrie', age: 45 } ]
+
+
+// Loop through people and return all the name of the people.
+  
+// Loop through people and return all ages.
+  
+// Loop through people and return what age they will be in 20 years. 
+  
